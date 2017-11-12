d8b266c923628cfa4a8ec831002febababeb8a37
laoxiao789 <laoxiao789@gmail.com>
2017-11-11 20:52:11 -0400

    v1

---
+++ src/HelloApp.java
@@ -0,0 +1,11 @@
+/**
+ * Created by huliwen on 2017-11-11.
+ */
+public class HelloApp {
+
+        public static void main(String[] args) {
+            System.out.println("Hello laoxiao789!");
+        }
+
+
+}
